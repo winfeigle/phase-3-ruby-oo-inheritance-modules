a05ee5578bc5460898832063af6ac9830e71da31
Win Feigle <win.feigle@gmail.com>
2022-10-01 15:28:19 -0600

    commit tree c1794832147ae3fc9475c160712fed2b691accd7

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
+require_relative './fancy_dance'
 require_relative './meta_dancing_module'
+
+class Dancer
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
